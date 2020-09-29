b9156df92bcc6bff637e3ad626a287f1b51385a2
Agung Maulana <iamaul@hotmail.com>
2020-09-30 01:55:35 +0700

    update blogs form

--- resources/views/v1/blogs/form.blade.php
+++ resources/views/v1/blogs/form.blade.php
@@ -145,7 +145,7 @@
         }
     });
 
-    @isset('{{ $blog["header_image"] }}')
+    @isset($blog['header_image'])
         $('#header_image_form').show();
         $('#profile_user_img').show();
         $('#header_image').addClass('hidden');
